2bb80da96a4f4f5f94fbb277e8787f9d2afb1234
Diana Badas <diana.badas@gmail.com>
2019-01-29 16:22:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,27 +9,18 @@ class Triangle
   end
 
   def kind
-    if @side1 <= 0 || @side2 <= 0 || @side_3 <= 0
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
-    elsif @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
     else
-      if @side1 != @side2 && @side2 != @side3 && @side1 != @side3
-        :scalene
-      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
-        :isosceles
-      elsif @side1 == @side2 && @side2 == @side3
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
       end
-    end
   end
 
   class TriangleError < StandardError
